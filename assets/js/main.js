a1e5bc807c8521737ddf59a207084e6db988777f
davmaene <davidmened@gmail.com>
2021-09-13 20:24:33 +0200

    up fnk

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const _calcNumSubscribes = (params) => {
-    const {i = new Number(), w = new Number(), r = new Number(), v = new Number(), l = new Number(), n = new Number(), } = params;
+    const { i = new Number(), w = new Number(), r = new Number(), v = new Number(), l = new Number(), n = new Number(), } = params;
     // i = new Number(), w = new Number(), r = new Number(), v = new Number(), N = new Number()
     // i : rapprt de l'interferance intra cellulaire sur l'interferance inter cellulaire
     // W : chip rate en kcps
