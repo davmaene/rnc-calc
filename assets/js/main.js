ebe6b65b872dd72060b0deb6581b88dadd859e48
davmaene <davidmened@gmail.com>
2021-09-13 20:11:43 +0200

    up link

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const _calcNumSubscribes = (i = new Number()) => {
+    // i : rapprt de l'interferance intra cellulaire sur l'interferance inter cellulaire
+    // W : chip rate en kcps
+    // R : débit pour un seul utilisateur
+    // V : voice factor
+    // N : nombre d'utilisateur actif dans une cellule
+}
